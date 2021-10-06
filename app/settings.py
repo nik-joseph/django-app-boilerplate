495b03cf411b33ca843737d7b9e0369cd06b2da0
Nikhil Kurian Joseph <nikhil.kurian.n3@gmail.com>
2021-10-06 13:03:01 +0100

    Settings refactor

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uzj)w8#6o3fl(ad$qr%h-ojz*dq4gfuyosiywq+j*$y7c%%z__'
+SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('APP_DEBUG', False)
 
 ALLOWED_HOSTS = []
 
